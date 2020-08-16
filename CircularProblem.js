a9b8ecbbc3ee34ba777bdf30c10b52f29431ac70
michaelbroussard <michael_broussard@msn.com>
2020-08-16 12:49:06 -0500

    adding circular linked list that utilizes linked list.

---
+++ CircularProblem.js
@@ -0,0 +1,29 @@
+//Create an algorithm that takes a series of number in a circle. Given a starting point, move around the circle given a number x. When you come to the xth number
+//remove it from the list and continue on, removing the xth number until one number remains.
+const circularLinkedList = require("circularlinkedlist")();
+
+module.exports = (function () {
+    let circularProblem = {}
+
+    circularProblem.run = function (listOfNumbers, numberToSkip) {
+        let numberOfNodesToSkip = numberToSkip;
+        let list = circularLinkedList.createCircularLinkedListFromDataArray(listOfNumbers);
+        if(list.getNodeCountFromList() === 1)
+            return this.head.data;
+
+        let currentNode = list.head;
+        let nodeToDelete;
+        while(list.getNodeCountFromList() !== 1) {
+            for (let i = 0; i < numberOfNodesToSkip; i++) {
+                currentNode = currentNode.nextNode;
+            }
+            nodeToDelete = currentNode;
+            currentNode = currentNode.nextNode;
+            list.deleteNode(nodeToDelete);
+        }
+
+        return currentNode.data;
+    }
+    return circularProblem;
+})();
+
---
+++ app.js
@@ -0,0 +1,3 @@
+const circularProblem = require("./CircularProblem");
+var theNumber = circularProblem.run([1,2], 3);
+console.log("The last number in the sequence is " + theNumber);
